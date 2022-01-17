b17b2c20ed4d6a486b74730008f27c6f014cb251
anc93 <reaganpang@yahoo.com>
2022-01-17 21:36:28 +0800

    initial skeleton

---
+++ src/main/java/com/hobis/api/HobisApiApplication.java
@@ -0,0 +1,13 @@
+package com.hobis.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobisApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HobisApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hobis/api/HobisApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hobis.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobisApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
